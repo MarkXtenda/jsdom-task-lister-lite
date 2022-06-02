3f15d3ffaf3cc6880167cf4a99833627953a0d20
MarkXtenda <mark.i2p.1998@gmail.com>
2022-06-02 17:42:48 +1000

    Final

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  let textField = document.querySelector("#new-task-description")
+  let submit = document.querySelector("#submit")
+  let tasks = document.querySelector("#tasks")
+  submit.addEventListener("click", (event)=>{
+      
+      let li = tasks.appendChild(document.createElement("li"))
+      li.innerHTML = textField.value
+      deleteBtn = document.createElement("button")
+      deleteBtn.className = "delete";
+      deleteBtn.innerHTML = "x"
+      li.appendChild(deleteBtn)
+
+      addEventListener("click", (deleteBtn) => {
+        console.log(deleteBtn.target.id)
+        if (deleteBtn.target.className === "delete") {
+          deleteBtn.target.parentElement.remove()
+        }
+      })
+
+      event.preventDefault()
+  })
+
+
 });
